63b4fd1020e5c389e46b23ca9d6f1e487df60710
Brian Yingling <brian_yingling@yahoo.com>
2013-01-30 16:41:55 -0500

    stuff

--- labpartners.rb
+++ labpartners.rb
@@ -26,23 +26,35 @@ students = ['Stephen B',
             'Jill Y'
           ]
 
-puts "How many groups ?"
+print "How many groups ?"
+
+#number of groups
 g_num = gets.chomp.to_i
+
+#size of class
 class_size = students.length
 
-per_group = class_size / g_num
-remainder = class_size % g_num
 
-g_num.times do
+#Members per group
+per_group = class_size / g_num      #people in each group - #25 people / 4 groups
+remainder = class_size % g_num      #number of people after division , people left over.
+
+g_num.times do                      # do this loop 4.times (number of group)
       print "GROUP: "
-      per_group.times do
+      per_group.times do            # Do this x.times for x amount of people per group.
             print "#{students.pop}, "
       end
-      if (remainder > 0) && (students.length <= remainder)
+      if (remainder > 0) && (students.length <= remainder)    # if there's a remainder and the length is less than students.length
             remainder.times do
                   print "#{students.pop}"
             end
       end
       puts " "
 end
+
+# (1..per_group).each do |x|
+
+#   students.each do |s|
+
+# end
 
