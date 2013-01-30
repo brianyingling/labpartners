7896ed17196bc17e8e0795383a6a31865f1d7480
Bryan W. Reid <bwreid@gmail.com>
2013-01-30 17:02:31 -0500

    ploading new version

--- labpartners.rb
+++ labpartners.rb
@@ -26,21 +26,63 @@ students = ['Stephen B',
             'Jill Y'
           ]
 
-puts "How many groups ?"
+print "How many groups would you like, Chylde? "
+
+students = students.shuffle
+
 g_num = gets.chomp.to_i
 class_size = students.length
+
 per_group = class_size / g_num
 remainder = class_size % g_num
 
+# ============================
+# VERSION ONE
+# ============================
+
+# full = []
+# start = 0
+
+# g_num.times do
+#       emp = []
+
+#       (start..(start + per_group)).each do |x|
+#             emp << students[x]
+#       end
+
+#       if (remainder > 0) && (students.length <= remainder)
+#             remainder.times do
+#                   emp << students
+#             end
+#       end
+
+#       full << emp
+#       print "#{emp} "
+#       puts " "
+#       start = start + per_group
+#       puts "#{start} - New Start"
+
+
+# end
+
+# puts "#{full} - New Array"
+
+# ============================
+# VERSION TWO - my fav
+# ============================
+
 g_num.times do
-      print "GROUP: "
+      print "GROUP >> "
       per_group.times do
-            print "#{students.pop}, "
+            print ": #{students.pop} "
       end
       if (remainder > 0) && (students.length <= remainder)
             remainder.times do
-                  print "#{students.pop}"
+                  print ": #{students.pop}"
             end
       end
       puts " "
 end
+
+puts "#{students.length} - Left in Array"
+
